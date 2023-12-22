143c122692b8eef3ba47317cc5612f64d5682940
Andrei Demichev <andyuson24@gmail.com>
2023-12-22 08:55:00 +0200

    выполнил 4 усложненное задание

--- script/script.js
+++ script/script.js
@@ -1,31 +1,15 @@
-const lang = 'en';
-const masRu = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-const masEn = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+const nameM = 111;
 
-if (lang === 'ru'){
-    console.log(masRu);
-}else{
-    console.log(masEn);
-};
 
-switch (lang) {
-    case 'ru':
-        console.log(masRu);
-        break;
-    case 'en':
-        console.log(masEn);
-        break;
-    default: 
-        console.log('Неверные данные');
-};
+function test(str) {
+    if (typeof str !== 'string') {
+    alert('Вы ввели не верное значение');
+    return;
+    }
 
-const mas = {
-	'ru':['пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс'],
-	'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
-};
-console.log(mas[lang]);
+    str = str.trim(); 
+    return str.length > 30 ? str.slice(0, 30) + '...' : str;
+}
 
-let namePerson = 'Артем';
+test(nameM);
 
-namePerson === 'Артем' ? console.log('Директор') : 
-namePerson === 'Александр' ? console.log('Преподователь') : console.log('Студент');
