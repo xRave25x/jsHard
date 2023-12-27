eb107b91b48bd2cf82eb4c1ffe90d8f9e2d0fa0c
Andrei Demichev <andyuson24@gmail.com>
2023-12-27 11:37:48 +0200

    попробовал более простой метод выполнения задания + реализовал вывод чисел 2 способами

--- script/script.js
+++ script/script.js
@@ -1,13 +1,28 @@
-const arr = [24443, 45678, 45234, 34578, 24567, 34597, 23417]
-let filteredArr = arr.filter(i => String(i)[0] == 2 || String(i)[0] == 4);
-
-const Natural = (x) => {
-    for (let i = 2; i * i <= x; i == 2 ? i++ : i += 2)
-        if (x % i == 0) return false;
-    return x > 1;
-}
-
-const res = [...Array(100)].reduce((a, _, i) => a.concat(Natural(i) ? `Делители числа ${i}: 1 и ${i}` : []), []).join('\n');
+const arr = ['24443', '45678', '45234', '34578', '24567', '34597', '23417']
+let filteredArr = arr.filter(i => (i)[0] == 2 || (i)[0] == 4); // через filter
 
 console.log(filteredArr);
-console.log(res);
+
+arr.forEach((item) => {
+    if (item.startsWith('2') || item.startsWith('4')) { // через цикл forEach с применением .startWith()
+        console.log(item);
+    }
+});
+
+
+for(let i = 2; i <= 100; i++){
+    let primeNum = true;
+
+    for(let j = 2; j < i; j++) {
+
+        if(i % j == 0) {
+            primeNum = false;
+            break;
+        }
+    }
+
+    if (primeNum) {
+        console.log(i, 'Делители этого числа: 1 и ' + i);
+    }
+}
+
