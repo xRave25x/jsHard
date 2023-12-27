380616f55d8d92a0946458fafcfb0d9d2f13784f
Andrei Demichev <andyuson24@gmail.com>
2023-12-27 09:03:56 +0200

    выполнил усложненное 5 задание

--- script/script.js
+++ script/script.js
@@ -1,9 +1,5 @@
 const arr = [24443, 45678, 45234, 34578, 24567, 34597, 23417]
-
 let filteredArr = arr.filter(i => String(i)[0] == 2 || String(i)[0] == 4);
-
-console.log(filteredArr);
-
 
 const Natural = (x) => {
     for (let i = 2; i * i <= x; i == 2 ? i++ : i += 2)
@@ -13,4 +9,5 @@ const Natural = (x) => {
 
 const res = [...Array(100)].reduce((a, _, i) => a.concat(Natural(i) ? `Делители числа ${i}: 1 и ${i}` : []), []).join('\n');
 
+console.log(filteredArr);
 console.log(res);
