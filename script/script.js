bf6bcef96ae2a54bbb4fb363459edede9dac33fe
Andrei Demichev <andyuson24@gmail.com>
2024-01-20 15:10:09 +0200

    выполнил 9 усложненное задание

--- script/script.js
+++ script/script.js
@@ -1,46 +1,66 @@
-const date = new Date();
-
 const dateBlock = document.getElementById('current_date_time_block');
 const secondDateBlock = document.getElementById('second_date_block');
 const currentDateBlock = document.getElementById('current_date_time_block2');
 
-const options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    weekday: 'long',
-    timezone: 'UTC',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric'
-};
 
-const dateUp = date.toLocaleString("ru", options);
+firstDateTime = () => {
+    
 
-secondDateBlock.innerHTML = dateUp;
+    const currentDatetime = new Date();
+    const day = currentDatetime.toLocaleString('default', { weekday: 'long' }) + ', ' + zero_first_format(currentDatetime.getDate());
+    const month = currentDatetime.toLocaleString('default', { month: 'long' });
+    const year = currentDatetime.getFullYear();
+    const hours = zero_first_format(currentDatetime.getHours());
+    const minutes = zero_first_format(currentDatetime.getMinutes());
+    const seconds = zero_first_format(currentDatetime.getSeconds());
 
-    function zero_first_format(value)
-    {
-        if (value < 10)
-        {
-            value='0'+value;
+    const getDeclination = (num, a, b, c) => {
+
+        if (num == 1 || num % 10 == 1 && num != 11) {
+        return a;
+        } else if ((num >= 2 && num <= 4) || (num % 10 >= 2 && num % 10 <= 4 && (num < 10 || num > 20))) {
+        return b;
+        } else {
+        return c;
         }
-        return value;
     }
-    function date_time()
+    const getDeclinationHours = (hours) => {
+        return getDeclination(hours, 'час', 'часа', 'часов')
+    }
+    const getDeclinationMinutes = (minutes) => {
+        return getDeclination(minutes, 'минута', 'минуты', 'минут')
+    }
+    const getDeclinationSeconds = (seconds) => {
+        return getDeclination(seconds, 'секунда', 'секунды', 'секунд')
+    }
+
+    return 'Сегодня ' + day+" "+month+" "+year+' года'+", "+hours+' '+getDeclinationHours(hours)+" "+minutes+' '+getDeclinationMinutes(minutes)+" "+seconds+' '+getDeclinationSeconds(seconds);
+}
+
+
+zero_first_format = (value) =>
+{
+    if (value < 10)
     {
-        const current_datetime = new Date();
-        const day = zero_first_format(current_datetime.getDate());
-        const month = zero_first_format(current_datetime.getMonth()+1);
-        const year = current_datetime.getFullYear();
-        const hours = zero_first_format(current_datetime.getHours());
-        const minutes = zero_first_format(current_datetime.getMinutes());
-        const seconds = zero_first_format(current_datetime.getSeconds());
-
-        return day+"."+month+"."+year+" "+hours+":"+minutes+":"+seconds;
+        value='0'+value;
     }
-    dateBlock.innerHTML = date_time();
+        return value;
+}
+date_time = () =>{
+        
+    const currentDatetime = new Date();
+    const day = zero_first_format(currentDatetime.getDate());
+    const month = zero_first_format(currentDatetime.getMonth()+1);
+    const year = currentDatetime.getFullYear();
+    const hours = zero_first_format(currentDatetime.getHours());
+    const minutes = zero_first_format(currentDatetime.getMinutes());
+    const seconds = zero_first_format(currentDatetime.getSeconds());
 
-    setInterval(()=> {
-        currentDateBlock.innerHTML = date_time();
-    }, 1000);
+    return day+"."+month+"."+year+" "+hours+":"+minutes+":"+seconds;
+}
+
+setInterval(()=> {
+    secondDateBlock.innerHTML = date_time();
+}, 1000);
+
+currentDateBlock.innerHTML = firstDateTime();
